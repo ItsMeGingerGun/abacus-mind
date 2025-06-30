42a2d5452468b67990c86f400756a0fe7180ec9d
ItsMeGingerGun <happysolutionsivakasi@gmail.com>
2025-06-30 18:49:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Serve static files from public directory
+// Serve static files from public directory with root 
 app.use(express.static(path.join(__dirname, './public'), {
   index: 'index.html'
 });
